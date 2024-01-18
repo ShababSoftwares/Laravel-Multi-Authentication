8970323f56e9db87ea7b86d6ddd84c41c2105a1c
Arif Shabab <ml.arifshabab@gmail.com>
2024-01-18 16:54:54 +0500

    Multi Auth Url Added

--- database/multiple-auth-db.sql
+++ database/multiple-auth-db.sql
@@ -2,7 +2,8 @@
 SQLyog Ultimate v11.5 (64 bit)
 MySQL - 10.4.22-MariaDB : Database - multi_auth_db
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -32,7 +33,7 @@ CREATE TABLE `admins` (
 
 /*Data for the table `admins` */
 
-insert  into `admins`(`id`,`name`,`email`,`password`,`is_super`,`remember_token`,`created_at`,`updated_at`) values (1,'Admin','admin@software.com','$2y$10$gpHKNrySXBiO1scSOyNRruc6zlbHbXXnNcqHKyZEmghJ8SXewbOtC',0,NULL,'2019-12-04 02:18:57','2019-12-04 02:18:57');
+insert  into `admins`(`id`,`name`,`email`,`password`,`is_super`,`remember_token`,`created_at`,`updated_at`) values (1,'Admin','admin@multiauth.com','$2y$10$gpHKNrySXBiO1scSOyNRruc6zlbHbXXnNcqHKyZEmghJ8SXewbOtC',0,NULL,'2019-12-04 02:18:57','2019-12-04 02:18:57');
 
 /*Table structure for table `clients` */
 
@@ -53,8 +54,8 @@ CREATE TABLE `clients` (
 
 /*Data for the table `clients` */
 
-insert  into `clients`(`id`,`name`,`email`,`password`,`is_editor`,`remember_token`,`created_at`,`updated_at`) values (1,'Client','client@software.com','$2y$10$uNXsMnaQL1LH5cikG3So4OkWd/SHnsoMTZT.cQAzawYtjlyfJ0ClC',0,NULL,'2019-12-04 02:18:25','2019-12-04 02:18:25');
-insert  into `clients`(`id`,`name`,`email`,`password`,`is_editor`,`remember_token`,`created_at`,`updated_at`) values (2,'Client','client2@software.com','$2y$10$MdrLpRvPAXo3lnyO4kD3LeJl8cZm95IbfsUUS6Pdp93VVjOZr8kh2',0,NULL,'2019-12-04 03:13:16','2019-12-04 03:13:16');
+insert  into `clients`(`id`,`name`,`email`,`password`,`is_editor`,`remember_token`,`created_at`,`updated_at`) values (1,'Client','client@multiauth.com','$2y$10$uNXsMnaQL1LH5cikG3So4OkWd/SHnsoMTZT.cQAzawYtjlyfJ0ClC',0,NULL,'2019-12-04 02:18:25','2019-12-04 02:18:25');
+insert  into `clients`(`id`,`name`,`email`,`password`,`is_editor`,`remember_token`,`created_at`,`updated_at`) values (2,'Client','client2@multiauth.com','$2y$10$MdrLpRvPAXo3lnyO4kD3LeJl8cZm95IbfsUUS6Pdp93VVjOZr8kh2',0,NULL,'2019-12-04 03:13:16','2019-12-04 03:13:16');
 
 /*Table structure for table `failed_jobs` */
 
@@ -104,7 +105,7 @@ CREATE TABLE `password_resets` (
 
 /*Data for the table `password_resets` */
 
-insert  into `password_resets`(`email`,`token`,`created_at`) values ('user@software.com','$2y$10$Ie.dm0bcNAh6IUgF4wL6pebKeHPoVVg75naMbC9HBRZPHw7c3/dEm','2019-12-04 02:54:42');
+insert  into `password_resets`(`email`,`token`,`created_at`) values ('user@multiauth.com','$2y$10$Ie.dm0bcNAh6IUgF4wL6pebKeHPoVVg75naMbC9HBRZPHw7c3/dEm','2019-12-04 02:54:42');
 
 /*Table structure for table `users` */
 
@@ -125,8 +126,8 @@ CREATE TABLE `users` (
 
 /*Data for the table `users` */
 
-insert  into `users`(`id`,`name`,`email`,`email_verified_at`,`password`,`remember_token`,`created_at`,`updated_at`) values (1,'User','user@software.com',NULL,'$2y$10$iz.6v3hr4C2F9Jin7CkvEe8JMYEKcty4jAxdQsaenASXBQdjkRHRq',NULL,'2019-12-04 02:40:11','2019-12-04 02:40:11');
-insert  into `users`(`id`,`name`,`email`,`email_verified_at`,`password`,`remember_token`,`created_at`,`updated_at`) values (2,'User','user2@software.com',NULL,'$2y$10$otztE.9RS7Ky//.aD0rpw.bR1.ZKogngptxIZ6xhFCqrebOhHnbE2',NULL,'2019-12-04 03:00:12','2019-12-04 03:00:12');
+insert  into `users`(`id`,`name`,`email`,`email_verified_at`,`password`,`remember_token`,`created_at`,`updated_at`) values (1,'User','user@multiauth.com',NULL,'$2y$10$iz.6v3hr4C2F9Jin7CkvEe8JMYEKcty4jAxdQsaenASXBQdjkRHRq',NULL,'2019-12-04 02:40:11','2019-12-04 02:40:11');
+insert  into `users`(`id`,`name`,`email`,`email_verified_at`,`password`,`remember_token`,`created_at`,`updated_at`) values (2,'User','user2@multiauth.com',NULL,'$2y$10$otztE.9RS7Ky//.aD0rpw.bR1.ZKogngptxIZ6xhFCqrebOhHnbE2',NULL,'2019-12-04 03:00:12','2019-12-04 03:00:12');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
